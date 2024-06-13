a5d7dfbcbcee431d3e44866a33d4fce6df583347
cgarcia <celigarcia.cba@gmail.com>
2024-06-13 20:10:54 -0300

    subo repo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import './App_fondo.css'
+
+function App() {
+ 
+let curso = 57785
+let nombre = "Celina"
+
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <h2>Estamos en el curso de coder</h2>
+      <h2>Curso : {curso}</h2>
+      <p>Hola soy: {nombre}</p>
+    </>
+  )
+} 
+
+export default App
